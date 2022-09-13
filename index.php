f18c27ac8b767b5fb292df763bae80efbd6d8a9f
PedroF-Pires <113388713+PedroF-Pires@users.noreply.github.com>
2022-09-13 17:35:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <html>
+<head></head>
+<body background="bandeira.jpg">
+
+<h1 align="center">Resumo Geral da Independência do Brasil</h1>
+
+<div align="left"><p>
+A independência do Brasil foi anunciada por d. Pedro às margens do riacho Ipiranga, em São Paulo, no dia 7 de setembro de 1822. 
+Esse foi o resultado do distanciamento entre brasileiros e portugueses a partir de 1820. As tentativas de recolonização do Brasil pelas 
+Cortes fizeram com que as ideias de independência ganhassem força na colônia.
+</p>
+
+<p>
+D. Pedro foi quem encabeçou esse processo, sendo aconselhado por José Bonifácio, defensor da transformação do Brasil 
+em uma monarquia constitucional não liberal. A independência do Brasil foi seguida das Guerras de Independência, 
+que se estenderam até o ano de 1824. Os portugueses só reconheceram nossa independência em 1825.
+</p>
+
+<img src="dpedro.jpg">
+</div>
+
+
+<div align="right"><p>
+A independência do Brasil foi um processo acelerado por conta de acontecimentos em Portugal no século XIX. 
+O contexto histórico de Portugal é fundamental para que possamos entender as transformações que aconteceram 
+no Brasil e que deram abertura para que a luta pela independência se iniciasse.
+</p>
+
+<p>
+O processo de independência não acabou em 1822, estendendo-se até 1825, quando os portugueses reconheceram a independência do Brasil. 
+Nesse período, o Brasil enfrentou uma guerra civil, uma vez que forças leais a Portugal levantaram-se em locais como Cisplatina e Bahia. 
+Esses conflitos ficaram conhecidos como Guerras de Independência e duraram até 1824.
+</p>
+
+<img src="independencia.jpg">
+</div>
+
+<div align="center"><p>
+Paralelamente aos conflitos internos, o Brasil precisava obter o reconhecimento internacional. 
+Oficialmente, os Estados Unidos são considerados o primeiro país a reconhecer a independência do Brasil, no ano de 1824. 
+Entretanto, novos estudos apontam que possivelmente a Argentina, já em 1823, havia feito esse reconhecimento. 
+O reconhecimento português, só veio em 1825.
+</p>
+
+<div align="center"><p>
+Além disso, a Constituinte brasileira foi formada e reuniu-se a partir de 1823. Uma Constituição foi outorgada em 1824, e o 
+Brasil estabeleceu-se como uma monarquia, com d. Pedro I tendo poderes absolutos. A escravidão foi mantida, uma vez que não 
+era interesse das elites, formadas pelos grandes fazendeiros, em abolir essa instituição. Esse período após a independência é 
+conhecido como Primeiro Reinado.
+</p>
+
+<img src="reinado.jpg">
+</div>
+
+</body>
+</html> 
+    </body>
+</html>
